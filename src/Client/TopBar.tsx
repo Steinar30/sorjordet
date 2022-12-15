6db671e17d0d2408cc5ba866fe8b36e1a5d7574b
steinar30 <steinarhansen352@gmail.com>
2022-12-15 23:34:25 +0100

    add drawer menu

--- src/Client/TopBar.tsx
+++ src/Client/TopBar.tsx
@@ -3,55 +3,103 @@ import {
     AppBar,
     Box,
     Button,
+    Drawer,
     IconButton,
+    List,
+    ListItem,
+    ListItemText,
     Toolbar,
     Typography
 } from "@suid/material";
 import { A } from "@solidjs/router";
-import { Show } from 'solid-js';
+import { createSignal, mapArray, Show } from 'solid-js';
 import { createMediaQuery } from "@solid-primitives/media";
 
 
 import logo from './farm-logo.svg';
+import styles from './TopBar.module.css';
+import { style } from "solid-js/web";
 
 export default function TopAppBar() {
     const isSmall = createMediaQuery("(max-width:600px)");
 
+    const [isOpen, setIsOpen] = createSignal(false);
+
+    
+
+    const navHome = () => 
+        <A href="/" class={styles.header_image_button} >
+            <img src={logo} alt="logo" />
+            <Typography variant="h6" class={styles.header_image_text} component="div">
+                Sørjordet
+            </Typography>
+        </A>
+
+    const navFields = <A class={styles.header_link} href="/fields">Jorder</A>
+    const navAbout  = <A class={styles.header_link} href="/about">Om siden</A>
+    const navLogin  = <A class={styles.header_link} href="/login">Logg inn</A>
+
     return (
         
-            <AppBar position="static">
+            <AppBar position="static" class={styles.header_container}>
                 <Toolbar>
-                    <Button href="/" >
-                        <img src={logo} alt="logo" style={{ "max-height": "48px", "max-width": "48px", "padding-left": "5px" }} />
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: "black", ml: "10px", mr: "10px"}}>
-                            Sørjordet
-                        </Typography>
-                    </Button>
+                    {navHome()}
 
                     <Show
-                        when={!isSmall()}
+                        when={isSmall()}
                         fallback={
-                            <IconButton
-                                size="large"
-                                edge="start"
-                                color="inherit"
-                                aria-label="menu"
-                                sx={{ ml: "auto", mr: "-10px" }}
-                                >
-                                <MenuIcon />
-                            </IconButton>
+                            <div style={{"margin-left":"auto"}}>
+                                {navFields}
+                                {navAbout}
+                                {navLogin}
+                            </div>
                         }
                     >
-                        <div style={{"margin-left":"auto"}}>
-                            <A style={{ "margin-right": "5px" }} href="/">Marker</A>
-                            <A style={{ "margin-right": "5px" }} href="/login">Logg inn</A>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            onClick={() => {
+                                setIsOpen(!isOpen());
+                            }}
+                            sx={{ ml: "auto", mr: "-10px" }}
+                        >
+                            <MenuIcon />
 
-                        </div>
-
+                        </IconButton>
+                        <Drawer
+                            class={styles.header_container}
+                            anchor="left"
+                            open={isOpen()}
+                            onClose={() => {
+                                setIsOpen(false);
+                            }}
+                            onClick={() => setIsOpen(false)}
+                        >
+                            <div 
+                                style={{padding:"10px"}}>
+                                {navHome()}
+                                <List>
+                                    <ListItem>
+                                        <ListItemText>
+                                            {navFields}
+                                        </ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemText>
+                                            {navAbout}
+                                        </ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemText>
+                                            {navLogin}
+                                        </ListItemText>
+                                    </ListItem>
+                                </List>
+                            </div>
+                        </Drawer>
                     </Show>
-
-
-                    
                 </Toolbar>
             </AppBar>
         
