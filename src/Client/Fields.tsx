9b9eb3b2625f1d2d5d10fbdf52f44f99489ec98f
Steinar Brenna Hansen <steinarhansen352@gmail.com>
2022-12-24 23:36:47 +0100

    Add better fieldslist rendering

--- src/Client/Fields.tsx
+++ src/Client/Fields.tsx
@@ -2,26 +2,53 @@ import { createResource, For } from "solid-js";
 import { getFarmFieldGroupsWithFields } from "./requests";
 
 import styles from './Admin.module.css';
-import { List, ListItem } from "@suid/material";
+import { Box, List, ListItem, ListItemIcon } from "@suid/material";
+import { FarmFieldGroup } from "./bindings/FarmFieldGroup";
+import { FarmField } from "./bindings/FarmField";
+import { NoEditMap } from "./Map";
+import { Circle } from "@suid/icons-material";
+
+export function RenderFieldsList(fieldGroups: [FarmFieldGroup, FarmField[]][] | undefined) {
+    return (
+        <List>
+            <ListItem>Liste over jorder etter gruppe:</ListItem>
+            <For each={fieldGroups}>{([fg,fields],i) => {
+                return (
+                    <List>
+                        <ListItem>
+                            {fg.name}
+                            <ListItemIcon>
+                                <Circle sx={{color: fg.draw_color, marginLeft:"10px"}}/>
+                            </ListItemIcon>
+                        </ListItem>
+                        <For each={fields}>{(field, j) => {
+                            return (
+                                <ListItem sx={{marginLeft:"10px"}}>
+                                    {field.name}
+                                </ListItem>
+                            )
+                        }}
+                        </For>
+                    </List>
+                )
+            }}
+            </For>
+        </List>
+    )
+}
 
 export default function FieldsList() {
-    const [farm_field_groups, { mutate, refetch }] = createResource(
+    const [farmFieldGroups, { mutate, refetch }] = createResource(
         getFarmFieldGroupsWithFields
     );
 
     return (
         <main class={styles.container}>
-            <List>
-                <For each={farm_field_groups()}>{(fg,i) => {
-                    return (
-                        <ListItem>
-                            {fg[0].name}
-                        </ListItem>
-                    )
-                }}
-                </For>
+            {RenderFieldsList(farmFieldGroups())}
 
-            </List>
+            <Box sx={{maxWidth: "600px", width:"90%", height:"400px"}}>
+                {NoEditMap}
+            </Box>
 
         </main>
     );
