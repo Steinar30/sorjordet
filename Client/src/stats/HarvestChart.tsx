743fa71610390f39692925e41c885ec809dd0bcd
Steinar <steinarhansen352@gmail.com>
2025-01-19 16:38:32 +0100

    fix rendering after backend changes

--- Client/src/stats/HarvestChart.tsx
+++ Client/src/stats/HarvestChart.tsx
@@ -1,4 +1,4 @@
-import { createResource, Show } from "solid-js";
+import { createMemo, createResource, Show } from "solid-js";
 import { SolidApexCharts } from "solid-apexcharts";
 import { HarvestAggregated } from "../../bindings/HarvestAggregated";
 
@@ -6,39 +6,68 @@ export default function HarvestChart() {
   const [harvestsByYear] = createResource<HarvestAggregated[]>(() =>
     fetch("/api/harvest_event/aggregated_harvests").then((a) => a.json()),
   );
+
+  const getAllDates = (x: HarvestAggregated[]) => {
+    const dates: Set<string> = new Set();
+    x.forEach(a => a.harvests.forEach(h => dates.add(h.date)))
+    // Sort date strings numerically
+    return Array.from(dates).sort((a, b) => {
+      const [ay, am] = a.split("-", 2);
+      const [by, bm] = b.split("-", 2);
+      if (ay === by) {
+        return Number(am) - Number(bm);
+      } else {
+        return Number(ay) - Number(by);
+      }
+    })
+  }
+
+  const alignSeriesToDates = (dates: string[], series: HarvestAggregated[]) => {
+    return series.sort((a, b) => a.type_id - b.type_id).map((agg) => {
+      return {
+        name: agg.type_name,
+        data: dates.map((date) =>
+          agg.harvests.find(a => a.date == date)?.total ?? 0
+        )
+      }
+    });
+  }
+
+  const dates = createMemo(() =>
+    getAllDates(harvestsByYear() ?? []), [harvestsByYear]
+  );
+  const chartSeries = createMemo(() =>
+    alignSeriesToDates(dates(), harvestsByYear() ?? []), [dates, harvestsByYear]
+  );
+
   return (
     <Show when={harvestsByYear()}>
-      {(harvestsByYear) => (
-        <SolidApexCharts
-          options={{
-            chart: {
-              type: "bar",
-              height: 300,
+      <SolidApexCharts
+        options={{
+          chart: {
+            type: "bar",
+            height: 300,
+          },
+          dataLabels: {
+            enabled: true,
+          },
+          xaxis: {
+            categories: dates(),
+          },
+          yaxis: {
+            title: {
+              text: "Number of bales",
             },
-            dataLabels: {
-              enabled: false,
-            },
-            xaxis: {
-              categories: harvestsByYear()[0].harvests.map((h) => h.date),
-            },
-            yaxis: {
-              title: {
-                text: "Total Bales",
-              },
-            },
-            fill: {
-              opacity: 0.8,
-            },
-          }}
-          series={harvestsByYear().map((h) => ({
-            name: h.type_name,
-            data: h.harvests.map((h) => h.total),
-          }))}
-          type="bar"
-          width="100%"
-          height="100%"
-        />
-      )}
+          },
+          fill: {
+            opacity: 0.8,
+          },
+        }}
+        series={chartSeries()}
+        type="bar"
+        width="100%"
+        height="100%"
+      />
     </Show>
   );
 }
