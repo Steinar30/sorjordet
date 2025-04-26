eb10181cf12843cf798d16526ce30af4d3ecae49
Steinar Brenna Hansen <steinarhansen352@gmail.com>
2025-04-26 20:05:22 +0200

    implement harvest forms

--- Client/src/admin/harvest/HarvestTypes.tsx
+++ Client/src/admin/harvest/HarvestTypes.tsx
@@ -3,6 +3,10 @@ import { createSignal, Show } from "solid-js";
 import { HarvestType } from "../../../bindings/HarvestType";
 import {
   Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
   IconButton,
   Table,
   TableBody,
@@ -10,14 +14,41 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  TextField,
 } from "@suid/material";
 import { Edit } from "@suid/icons-material";
+import { prepareAuth } from "../../requests";
+
+// apicalls dont return a new object, using refetch instead
+const updateHarvestType = (harvestType: HarvestType) => {
+  const authHeaders = prepareAuth(true);
+  if (authHeaders === null) {
+    console.log("not allowed to post without bearer token");
+    return null;
+  }
+  return fetch(`/api/harvest_type/${harvestType.id}`, {
+    method: "PATCH",
+    body: JSON.stringify(harvestType),
+    headers: authHeaders
+  });
+};
+
+const createHarvestType = (harvestType: HarvestType) => {
+  const authHeaders = prepareAuth(true);
+  if (authHeaders === null) {
+    console.log("not allowed to post without bearer token");
+    return null;
+  }
+  return fetch(`/api/harvest_type`, {
+    method: "POST",
+    body: JSON.stringify(harvestType),
+    headers: authHeaders
+  });
+};
 
 export default function HarvestTypes() {
-  const [showAddForm, setShowAddForm] = createSignal(false);
-  const [editForm, setEditForm] = createSignal<HarvestType | undefined>(
-    undefined,
-  );
+  const [addForm, setAddForm] = createSignal<HarvestType | undefined>(undefined);
+  const [editForm, setEditForm] = createSignal<HarvestType | undefined>(undefined);
 
   const harvestTypes = createQuery<HarvestType[]>(() => ({
     queryKey: ["harvest_types"],
@@ -33,12 +64,76 @@ export default function HarvestTypes() {
         width: "calc(100% - 40px)",
       }}
     >
+      <Dialog
+        open={addForm() !== undefined || editForm() !== undefined}
+        onClose={() => addForm() != undefined ? setAddForm(undefined) : setEditForm(undefined)}
+      >
+        <DialogTitle style={{ "padding-bottom": "" }}>
+          <Show when={editForm()} fallback={<p>Add New type</p>}>
+            <p>Update "{editForm()?.name}</p>
+          </Show>
+        </DialogTitle>
+        <DialogContent style={{ "padding-top": "10px" }}>
+          <Show when={addForm()}>
+            <TextField
+              id="name"
+              label="Name"
+              variant="outlined"
+              size="small"
+              value={addForm()?.name}
+              onChange={(x) => setAddForm({ id: -1, name: x.target.value })}
+            />
+          </Show>
+          <Show when={editForm()}>
+            <TextField
+              id="name"
+              label="Name"
+              variant="outlined"
+              size="small"
+              value={editForm()?.name}
+              onChange={(x) => setEditForm({ id: editForm()!.id, name: x.target.value })}
+            />
+          </Show>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => addForm() ? setAddForm(undefined) : setEditForm(undefined)}>Cancel</Button>
+          <Show when={addForm()}>
+            {(toAdd =>
+              <Button
+                variant="contained"
+                onClick={async () => {
+                  const n = await createHarvestType(toAdd());
+                  if (n?.ok) {
+                    setAddForm(undefined);
+                    harvestTypes.refetch();
+                  }
+                }}
+              >
+                Save
+              </Button>
+            )}
+          </Show>
+          <Show when={editForm()}>
+            {update => (<Button
+              variant="contained"
+              onClick={async () => {
+                const n = await updateHarvestType(update());
+                if (n?.ok) {
+                  setEditForm(undefined);
+                  harvestTypes.refetch();
+                }
+              }}
+            >
+              Save
+            </Button>)}
+          </Show>
+        </DialogActions>
+      </Dialog>
       <div style={{ display: "flex", "justify-content": "space-between" }}>
         <Button
           variant="contained"
           sx={{ textWrap: "nowrap" }}
-          onClick={() => setShowAddForm(true)}
-          disabled
+          onClick={() => setAddForm({ id: -1, name: "" })}
         >
           New type
         </Button>
@@ -60,7 +155,6 @@ export default function HarvestTypes() {
                   <TableCell>{harvestType.name}</TableCell>
                   <TableCell>
                     <IconButton
-                      disabled
                       onClick={() => setEditForm(harvestType)}
                     >
                       <Edit />
